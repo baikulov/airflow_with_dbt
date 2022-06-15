5952f724f18797b18bb19bd84c52d10b49569bf0
baikulov <baikulov.ant@gmail.com>
2022-06-15 10:21:00 +0400

    add telegram variables

--- dags/scripts/telegram_webhook.py
+++ dags/scripts/telegram_webhook.py
@@ -1,7 +1,8 @@
 def telegramm_alert(context):
     import requests
-    token = "953719854:AAF8yberWRp-tMH5BWrFk1NilnJeI9vJpZg"  # токен бота в телеге
-    chat_id = "-537128908"  # номер чата с ботом, можно получить внутри телеги
+    import os
+    token = os.environ.get('TELEGRAM_TOKEN') # токен бота в телеге
+    chat_id = os.environ.get('TELEGRAM_CHAT_ID')  # номер чата с ботом, можно получить внутри телеги
     error_context = context["task_instance"]
     url = "https://api.telegram.org/bot{}/sendMessage"
 
